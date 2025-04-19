8ec670628c0601b6c3e9354792fc52d8c75f886d
zarkO <asminzzzz0@gmail.com>
2025-04-19 20:12:17 +0300

    Add files via upload

---
+++ two_forms_one_app/two_forms_one_app/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace two_forms_one_app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2=new Form2();
+            frm2.frm1 = this;
+            frm2.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ two_forms_one_app/two_forms_one_app/Form2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace two_forms_one_app
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public Form frm1;
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            frm1.Show();
+        }
+    }
+}
