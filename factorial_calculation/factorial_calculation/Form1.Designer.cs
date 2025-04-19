effcf965e4784753e04cb59dbc0592478363b280
zarkO <asminzzzz0@gmail.com>
2025-04-19 19:21:31 +0300

    Add files via upload

---
+++ factorial_calculation/factorial_calculation/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace factorial_calculation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_n = new System.Windows.Forms.TextBox();
+            this.txt_r = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(10, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "r";
+            // 
+            // txt_n
+            // 
+            this.txt_n.Location = new System.Drawing.Point(32, 39);
+            this.txt_n.Name = "txt_n";
+            this.txt_n.Size = new System.Drawing.Size(100, 20);
+            this.txt_n.TabIndex = 2;
+            // 
+            // txt_r
+            // 
+            this.txt_r.Location = new System.Drawing.Point(32, 71);
+            this.txt_r.Name = "txt_r";
+            this.txt_r.Size = new System.Drawing.Size(100, 20);
+            this.txt_r.TabIndex = 3;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Location = new System.Drawing.Point(32, 98);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(41, 13);
+            this.lblSonuc.TabIndex = 4;
+            this.lblSonuc.Text = "Sonuç:";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(32, 115);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(75, 23);
+            this.btnHesapla.TabIndex = 5;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txt_r);
+            this.Controls.Add(this.txt_n);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_n;
+        private System.Windows.Forms.TextBox txt_r;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Button btnHesapla;
+    }
+}
+
---
+++ factorial_calculation/factorial_calculation/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace factorial_calculation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Faktoriyel(int sayi1)
+        {
+            int carpim = 1;
+            for(int i=1; i<=sayi1;i++)
+            {
+                carpim *= i;
+            }
+            return carpim;
+        }
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            int n=Convert.ToInt32(txt_n.Text);
+            int r=Convert.ToInt32(txt_r.Text);
+            int sonuc=Faktoriyel(n)/(Faktoriyel(r)*Faktoriyel(n-r));
+            lblSonuc.Text = "Sonuç: " + sonuc;
+        }
+    }
+}
