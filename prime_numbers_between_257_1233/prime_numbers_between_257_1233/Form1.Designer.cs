6c0e29fffff4540448af5b66e5c5ab3dd9ee6d39
zarkO <asminzzzz0@gmail.com>
2025-04-19 18:46:15 +0300

    Add files via upload

---
+++ prime_numbers_between_257_1233/prime_numbers_between_257_1233/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace prime_numbers_between_257_1233
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListele = new System.Windows.Forms.Button();
+            this.lstAsalSayi = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnListele
+            // 
+            this.btnListele.Location = new System.Drawing.Point(123, 77);
+            this.btnListele.Name = "btnListele";
+            this.btnListele.Size = new System.Drawing.Size(75, 23);
+            this.btnListele.TabIndex = 0;
+            this.btnListele.Text = "Listele";
+            this.btnListele.UseVisualStyleBackColor = true;
+            this.btnListele.Click += new System.EventHandler(this.btnListele_Click);
+            // 
+            // lstAsalSayi
+            // 
+            this.lstAsalSayi.FormattingEnabled = true;
+            this.lstAsalSayi.Location = new System.Drawing.Point(205, 77);
+            this.lstAsalSayi.Name = "lstAsalSayi";
+            this.lstAsalSayi.Size = new System.Drawing.Size(120, 316);
+            this.lstAsalSayi.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstAsalSayi);
+            this.Controls.Add(this.btnListele);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListele;
+        private System.Windows.Forms.ListBox lstAsalSayi;
+    }
+}
+
---
+++ prime_numbers_between_257_1233/prime_numbers_between_257_1233/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace prime_numbers_between_257_1233
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnListele_Click(object sender, EventArgs e)
+        {
+            lstAsalSayi.Items.Clear();
+            for (int i = 257; i < 1233; i++)
+            {
+                if (asalSayi(i))
+                {
+                    lstAsalSayi.Items.Add(i);
+                }
+            }
+        }
+
+        private bool asalSayi(int sayi)
+        {
+            if (sayi <2) return false;
+
+            for (int i = 2; i*i <=sayi; i++)
+            {
+                if (sayi%i==0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
